587342723e97778bbc5675c38574736c4c8be04c
josephurrego <josephdavidurrego07@gmail.com>
2025-02-10 20:54:27 -0500

    cambiar a 0.0.0.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const {client} = require('./wwebApp')
 
-const host = 'localhost';
+const host = '0.0.0.0';
 const port = 8000;
 
 const server = http.createServer((req,res)=>{
